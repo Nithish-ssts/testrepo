c0543a0013cf4c0d06427dfb674eb2b154edb7a6
Nithish-ssts <nithish.reddy@sstsinc.com>
2024-01-19 18:03:25 +0530

    nithish

--- finddevice.sh
+++ finddevice.sh
@@ -6,6 +6,20 @@ if [[ $adb_output == *device* ]]; then
     echo "Connected devices:"
     # Print the connected devices
     echo "$adb_output" | awk '$2=="device"{print $1}'
+    apk_path="/home/radmin/Downloads/zomat0(1).apk"
+    # Install APK on all connected devices
+    while read -r device; do
+        echo "Installing APK on device $device"
+        adb -s $device install -r $apk_path
+
+        # Check the result of the installation
+        if [ $? -eq 0 ]; then
+            echo "Installation successful on device $device"
+        else
+            echo "Installation failed on device $device"
+        fi
+    done <<< "$(echo "$adb_output" | awk '$2=="device"{print $1}')"
+
 else
     echo "No device is connected to the PC."
 fi
